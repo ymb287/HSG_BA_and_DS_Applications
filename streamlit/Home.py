db90d96c70008d38a7515dc76619c814028fb2dd
ymb287 <yannik.biebert@gmail.com>
2024-11-28 12:51:29 +0100

    Chosen points

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -5,17 +5,41 @@ from streamlit_folium import st_folium
 # Sidebar for navigation
 page = st.sidebar.radio("Select a Page", ["Home", "Page 1", "Page 2"])
 
-
 st.title("Home Page: St. Gallen Map")
 
-
 # Create a folium map centered around St. Gallen, Switzerland
-m = folium.Map(location=[47.423, 9.376], zoom_start=13)
+m = folium.Map(location=[-37.808, 144.966], zoom_start=15, tiles="CartoDB positron")
 
-# Add 3 markers with popups
-folium.Marker([47.423, 9.376], popup="Marker 1: St. Gallen").add_to(m)
-folium.Marker([47.420, 9.380], popup="Marker 2: University of St. Gallen").add_to(m)
-folium.Marker([47.430, 9.390], popup="Marker 3: Abbey of St. Gall").add_to(m)
+# Add 3 markers with custom icons
+folium.Marker(
+    location=[-37.814442, 144.966106],
+    popup="Little Collins St-Swanston St (East)",
+    icon=folium.Icon(color="blue", icon="info-sign")
+).add_to(m)
+
+folium.Marker(
+    location=[-37.811042, 144.964422],
+    popup="Melbourne Central",
+    icon=folium.Icon(color="green", icon="shopping-cart", prefix="fa")
+).add_to(m)
+
+folium.Marker(
+    location=[-37.811601, 144.968333],
+    popup="Chinatown-Lt Bourke St (South)",
+    icon=folium.Icon(color="red", icon="cutlery", prefix="fa")
+).add_to(m)
+
+folium.Marker(
+    location=[-37.811053, 144.966677],
+    popup="Lonsdale St (South)",
+    icon=folium.Icon(color="purple", icon="road", prefix="fa")
+).add_to(m)
+
+folium.Marker(
+    location=[-37.802133, 144.966569],
+    popup="Lygon St (West)",
+    icon=folium.Icon(color="orange", icon="university", prefix="fa")
+).add_to(m)
 
 # Display the map in Streamlit
 st_folium(m, width=700)
